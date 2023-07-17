7f6b7163a7ec8e3e1ade1835275a884b7e9cd845
nde95 <ndennison268@gmail.com>
2023-07-17 14:10:12 -0400

    setting up basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import './App.css'
-import { Header, Footer } from './Components/Layout'
+import './App.css';
+import { Header, Footer } from './Components/Layout';
+import { Home, NotFound } from './Pages';
+import { Routes, Route } from "react-router-dom"
 
 function App() {
+
 
   return (
     <>
     <Header />
-    Main Components 
+      <div className='pb-5'>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
     <Footer />
     </>
   )
