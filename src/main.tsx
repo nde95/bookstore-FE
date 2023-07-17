3450843fc1f9c1fb514ff26f41a6780286045481
nde95 <ndennison268@gmail.com>
2023-07-17 13:58:44 -0400

    injecting routing to app

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import './index.css'
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.js"
 import "bootstrap-icons/font/bootstrap-icons.css"
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
