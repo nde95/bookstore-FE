6e8d95265a59b0011a04ff7c02b5a0e7a70c9b45
nde95 <ndennison268@gmail.com>
2023-07-17 16:49:18 -0400

    adding product details on dedicated page

--- src/Pages/ProductDetails.tsx
+++ src/Pages/ProductDetails.tsx
@@ -1,21 +1,35 @@
-import { productsModel } from "../Interfaces";
+import { useParams } from "react-router-dom"
+import { useGetProductItemByIdQuery } from "../APIs/productItemAPI";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-interface Props {
-    productItem: productsModel;
-}
+const ProductDetails = () => {
+    const { productItemId } = useParams();
+    const { data, isLoading } = useGetProductItemByIdQuery(productItemId);
+    const [quantity, setQuantity] = useState(1);
 
-const ProductDetails = (props: Props) => {
+    const handleQuantity = (counter: number) => {
+      let newQuantity = quantity + counter
+      if (newQuantity == 0){
+        newQuantity = 1;
+      }
+      setQuantity(newQuantity)
+      return;
+    }
+
     return (
         <div className="container pt-4 pt-md-5">
-        <div className="row">
+        {!isLoading? (
+        <>
+          <div className="row">
           <div className="col-7">
-            <h2 className="text-success">{props.productItem.name}</h2>
+            <h2 className="text-success">{data.result?.name}</h2>
             <span>
               <span
                 className="badge text-bg-dark pt-2"
                 style={{ height: "40px", fontSize: "20px" }}
               >
-                {props.productItem.category}
+                {data.result?.category}
               </span>
             </span>
             <span>
@@ -23,23 +37,27 @@ const ProductDetails = (props: Props) => {
                 className="badge text-bg-light pt-2"
                 style={{ height: "40px", fontSize: "20px" }}
               >
-                {props.productItem.specialTag}
+                {data.result?.specialTag}
               </span>
             </span>
             <p style={{ fontSize: "20px" }} className="pt-2">
-              {props.productItem.description}
+              {data.result?.description}
             </p>
-            <span className="h3">$10</span> &nbsp;&nbsp;&nbsp;
+            <span className="h3">${data.result?.price}</span> &nbsp;&nbsp;&nbsp;
             <span
               className="pb-2  p-3"
               style={{ border: "1px solid #333", borderRadius: "30px" }}
             >
-              <i
+              <i onClick={() => {
+                handleQuantity(-1);
+              }}
                 className="bi bi-dash p-1"
                 style={{ fontSize: "25px", cursor: "pointer" }}
               ></i>
-              <span className="h3 mt-3 px-3">XX</span>
-              <i
+              <span className="h3 mt-3 px-3">{quantity}</span>
+              <i onClick={() => {
+                handleQuantity(+1);
+              }}
                 className="bi bi-plus p-1"
                 style={{ fontSize: "25px", cursor: "pointer" }}
               ></i>
@@ -52,21 +70,28 @@ const ProductDetails = (props: Props) => {
               </div>
   
               <div className="col-5 ">
-                <button className="btn btn-secondary form-control">
+                <Link to="/" className="btn btn-secondary form-control">
                   Back to Home
-                </button>
+                </Link>
               </div>
             </div>
           </div>
           <div className="col-5">
             <img
-              src={props.productItem.image}
+              src={data.result?.image}
               width="100%"
               style={{ borderRadius: "50%" }}
               alt="No content"
             ></img>
           </div>
         </div>
+        </>
+        ) : (
+          <div className="d-flex justify-content-center" style={{ width: "100%" }}>
+            <div>Loading, Please Wait...</div>
+          </div>
+        )}
+
       </div>
     )
 }
