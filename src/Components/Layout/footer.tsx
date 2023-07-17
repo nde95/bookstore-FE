5d4ee582d10dd4377305ee99896fac6c8816de91
nde95 <ndennison268@gmail.com>
2023-07-17 12:17:01 -0400

    sticky footer

---
+++ src/Components/Layout/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="footer fixed-bottom text-center p-3 bg-dark text-white">
+            &copy; Made with <i className="bi bi-heart-fill"></i> by nde95
+            </div>
+    )
+}
+
+export default Footer; 
