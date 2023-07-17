d4d19efef051528f858d0824acfdde3a1c632438
nde95 <ndennison268@gmail.com>
2023-07-17 14:11:10 -0400

    home page container for product mapping

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,13 @@
+import { ProductList } from "../Components/Page/ProductItems";
+
+const Home = () => {
+    return (
+        <div>
+            <div className="container p-2">
+                <ProductList />
+            </div>
+        </div>
+    )
+}
+
+export default Home;
