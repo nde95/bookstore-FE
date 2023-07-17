9c965139860b9fe521a227b74d748baca864d72b
nde95 <ndennison268@gmail.com>
2023-07-17 16:48:58 -0400

    reworking fetch to rely on new redux store

--- src/Components/Page/ProductItems/ProductList.tsx
+++ src/Components/Page/ProductItems/ProductList.tsx
@@ -1,21 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { productsModel } from '../../../Interfaces';
 import { ProductCard } from '.';
+import { useGetProductItemsQuery } from '../../../APIs/productItemAPI';
+import { useDispatch } from "react-redux";
+import { setProductItem } from '../../../Storage/Redux/productSlice';
 
 const ProductList = () => {
-    const [productItems, setProductItems] = useState<productsModel[]>([]);
+   // const [productItems, setProductItems] = useState<productsModel[]>([]);
+  const dispatch = useDispatch();
+  const { data, isLoading } = useGetProductItemsQuery(null);
 
     useEffect(() => {
-      fetch("https://ndebookstoreapi.azurewebsites.net/api/ProductItem").then((response) => response.json()).then((data) => {
-        console.log(data);
-        setProductItems(data.result);
-      });
-    }, []);
+      if (!isLoading){
+        dispatch(setProductItem(data.result));
+      }
+    }, [isLoading]);
   
+    if(isLoading){
+      return (
+        <div>Loading, Please Wait...</div>
+      )
+    }
 
     return (
         <div className='container row'>
-          {productItems.length >0 && productItems.map((productItem, index) => (
+          {data.result.length >0 && data.result.map((productItem : productsModel, index : number) => (
             <ProductCard productItem={productItem} key={index} />
           ))}
         </div>
