278e5dd275935fa8b8367f4843a059253211af42
nde95 <ndennison268@gmail.com>
2023-07-19 17:15:18 -0400

    adding order details on cart summary

--- src/Pages/ShoppingCart.tsx
+++ src/Pages/ShoppingCart.tsx
@@ -1,4 +1,4 @@
-import { CartSummary } from "../Components/Page/Cart";
+import { CartSummary, UserDetails } from "../Components/Page/Cart";
 
 const ShoppingCart = () => {
     return (
@@ -6,7 +6,9 @@ const ShoppingCart = () => {
             <div className="col-lg-6 col-12" style={{ fontWeight: 300 }}>
                 <CartSummary />
             </div>
-            <div className="col-lg-6 col-12 p-4">User Details</div>
+            <div className="col-lg-6 col-12 p-4">
+                <UserDetails />
+            </div>
         </div>
     )
 }
