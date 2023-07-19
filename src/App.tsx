b4ac20f1da82b1122316b68a7249993499579442
nde95 <ndennison268@gmail.com>
2023-07-19 16:08:25 -0400

    fetching cart data on app load/data refresh

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,23 @@ import './App.css';
 import { Header, Footer } from './Components/Layout';
 import { Home, NotFound, ProductDetails } from './Pages';
 import { Routes, Route } from "react-router-dom"
+import { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import { useGetShoppingCartQuery } from './APIs/shoppingCartAPI';
+import { setShoppingCart } from './Storage/Redux/shoppingCartSlice';
+
 
 function App() {
+  const dispatch = useDispatch();
 
+  const { data, isLoading } = useGetShoppingCartQuery("9d6a4d87-b61c-4452-8873-29c1d274367e");
+
+  useEffect(() => {
+    if (!isLoading){
+      console.log(data.result);
+      dispatch(setShoppingCart(data.result?.cartItems))
+    }
+  }, [data]);
 
   return (
     <>
