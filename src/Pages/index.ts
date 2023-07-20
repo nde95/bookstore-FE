2a778eb7854d9b2a497f37118f1d4f97ff9bd37b
nde95 <ndennison268@gmail.com>
2023-07-20 13:18:45 -0400

    login and register pages

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -2,5 +2,7 @@ import Home from "./Home"
 import NotFound from "./NotFound"
 import ProductDetails from "./ProductDetails"
 import ShoppingCart from "./ShoppingCart"
+import Login from "./Login"
+import Register from "./Register"
 
-export {Home, ShoppingCart, NotFound, ProductDetails}
+export {Home, ShoppingCart, NotFound, ProductDetails, Login, Register}
