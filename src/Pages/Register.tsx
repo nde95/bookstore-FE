f53befb0f2dcd7583570edb072ef7444e7e1cab0
nde95 <ndennison268@gmail.com>
2023-07-20 15:15:33 -0400

    implementing local state and validation for form

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+import { InputHelper } from "../Helper";
+
 const Register = () => {
+  const [loading, isLoading] = useState(false);
+  const [userInput, setUserInput] = useState({
+    userName: "",
+    password: "",
+    role: "customer",
+    name: "",
+  })
+
+  const handleUserInput = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const tempData = InputHelper(e, userInput);
+    setUserInput(tempData);
+  }
+
     return (
         <div className="container text-center">
         <form method="post">
@@ -10,6 +26,9 @@ const Register = () => {
                 className="form-control"
                 placeholder="Enter Username"
                 required
+                value={userInput.userName}
+                onChange={handleUserInput}
+                name="userName"
               />
             </div>
             <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
@@ -18,6 +37,9 @@ const Register = () => {
                 className="form-control"
                 placeholder="Enter Name"
                 required
+                value={userInput.name}
+                onChange={handleUserInput}
+                name="name"
               />
             </div>
             <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
@@ -26,14 +48,10 @@ const Register = () => {
                 className="form-control"
                 placeholder="Enter Password"
                 required
+                value={userInput.password}
+                onChange={handleUserInput}
+                name="password"
               />
-            </div>
-            <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
-              <select className="form-control form-select" required>
-                <option value="">--Select Role--</option>
-                <option value="customer">Customer</option>
-                <option value="admin">Admin</option>
-              </select>
             </div>
           </div>
           <div className="mt-5">
