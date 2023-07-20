f19517fc75596944ba3e1ffef4e802816c21ce93
nde95 <ndennison268@gmail.com>
2023-07-20 15:19:35 -0400

    interface for integrating api response

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -2,5 +2,6 @@ import productsModel from "./productsModel";
 import shoppingCartModel from "./shoppingCartModel";
 import cartItemModel from "./cartItemModel";
 import userModel from "./userModel";
+import apiResponse from "./apiResponse";
 
-export type {productsModel, userModel, shoppingCartModel, cartItemModel}
+export type {productsModel, userModel, apiResponse, shoppingCartModel, cartItemModel}
