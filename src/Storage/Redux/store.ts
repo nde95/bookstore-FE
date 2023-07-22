f668ba8648198d5ca4b6d002d2e6cae9025fac6e
nde95 <ndennison268@gmail.com>
2023-07-22 12:17:53 -0400

    registering payment api in store

--- src/Storage/Redux/store.ts
+++ src/Storage/Redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit/dist";
 import { productItemReducer } from "./productSlice";
-import { authApi, productItemApi, shoppingCartApi } from "../../APIs";
+import { authApi, paymentApi, productItemApi, shoppingCartApi } from "../../APIs";
 import { shoppingCartReducer } from "./shoppingCartSlice";
 import { authReducer } from "./authSlice";
 
@@ -12,8 +12,9 @@ const store = configureStore({
         [productItemApi.reducerPath]: productItemApi.reducer,
         [shoppingCartApi.reducerPath]: shoppingCartApi.reducer,
         [authApi.reducerPath]: authApi.reducer,
+        [paymentApi.reducerPath]: paymentApi.reducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productItemApi.middleware).concat(shoppingCartApi.middleware).concat(authApi.middleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productItemApi.middleware).concat(shoppingCartApi.middleware).concat(authApi.middleware).concat(paymentApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
