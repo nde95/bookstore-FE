e6e30e7cc5451ab7f6eac6113691b5323ebf110f
nde95 <ndennison268@gmail.com>
2023-07-24 14:18:04 -0400

    static order details

--- src/Utils/SD.ts
+++ src/Utils/SD.ts
@@ -2,3 +2,12 @@ export enum SD_Roles {
     ADMIN = "admin",
     CUSTOMER = "customer",
 }
+
+export enum SD_Status {
+    PENDING = "Pending",
+    CONFIRMED = "Confirmed",
+    HANDED_TO_CARRIER = "Handed to carrier",
+    SHIPPED = "Shipped",
+    DELIVERED = "Delivered",
+    CANCELLED = "Cancelled",
+}
