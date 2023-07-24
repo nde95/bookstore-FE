515ae31f78db5f35b841f3c866180a4ea62f2e09
nde95 <ndennison268@gmail.com>
2023-07-24 14:35:07 -0400

    passing orders to backend api

--- src/Components/Page/Payment/PaymentForm.tsx
+++ src/Components/Page/Payment/PaymentForm.tsx
@@ -1,13 +1,17 @@
 import {PaymentElement, useStripe, useElements} from '@stripe/react-stripe-js';
 import { useState } from "react";
 import { toastNotify } from '../../../Helper';
+import { orderSummaryProps } from '../Order/orderSummaryProps';
+import { apiResponse, cartItemModel } from '../../../Interfaces';
+import { useCreateOrderMutation } from '../../../APIs/orderApi';
+import { SD_Status } from '../../../Utils/SD';
 
 
-const PaymentForm = () => {
+const PaymentForm = ({ data, userInput }: orderSummaryProps) => {
   const stripe = useStripe();
   const elements = useElements();
   const [isProcessing, setIsProcessing] = useState(false);
-
+  const [createOrder] = useCreateOrderMutation();
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -29,6 +33,33 @@ const PaymentForm = () => {
       setIsProcessing(false);
     } else {
       console.log(result);
+
+
+      let subTotal = 0;
+      let totalItems = 0;
+      const orderDetailsDTO: any = [];
+      data.cartItems.forEach((item: cartItemModel) => {
+        const tempOrderDetail: any = {};
+        tempOrderDetail["productItemId"] = item.productItem?.id;
+        tempOrderDetail["quantity"] = item.quantity;
+        tempOrderDetail["itemName"] = item.productItem?.name;
+        tempOrderDetail["price"] = item.productItem?.price;
+        orderDetailsDTO.push(tempOrderDetail);
+        subTotal += (item.quantity! * item.productItem?.price!)
+        totalItems += item.quantity!;
+      });
+
+      const response: apiResponse = await createOrder({
+        pickupName: userInput.name,
+        pickupPhoneNumber: userInput.phoneNumber,
+        pickupEmail: userInput.email,
+        totalItems: totalItems,
+        orderTotal: subTotal,
+        orderDetailsDTO: orderDetailsDTO,
+        stripePaymentIntentId: data.stripePaymentIntentId,
+        applicationUserId: data.userId,
+        status: result.paymentIntent.status === "succeeded" ? SD_Status.CONFIRMED : SD_Status.PENDING
+      });
     }
   };
   return (
