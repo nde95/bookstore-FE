65b8e61158aa9550814fb9fef7a19ab43fe20e27
nde95 <ndennison268@gmail.com>
2023-07-25 15:23:20 -0400

    updating admin panel with new routes

--- src/Components/Layout/header.tsx
+++ src/Components/Layout/header.tsx
@@ -4,124 +4,176 @@ import Logo from "../../assets/nde-alt.png";
 import { NavLink, useNavigate } from "react-router-dom";
 import { RootState } from "../../Storage/Redux/store";
 import { emptyUserState, setLoggedInUser } from "../../Storage/Redux/authSlice";
-
+import { SD_Roles } from "../../Utils/SD";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const shoppingCartFromStore : cartItemModel[] = useSelector(
-    (state : RootState) => state.shoppingCartStore.cartItems ?? []
-)
+  const shoppingCartFromStore: cartItemModel[] = useSelector(
+    (state: RootState) => state.shoppingCartStore.cartItems ?? []
+  );
 
-    const userData : userModel = useSelector((state: RootState) => state.authStore);
+  const userData: userModel = useSelector(
+    (state: RootState) => state.authStore
+  );
 
-    const handleLogout = () => {
-      localStorage.removeItem("token");
-      dispatch(setLoggedInUser({...emptyUserState}))
-      navigate("/");
-    }
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    dispatch(setLoggedInUser({ ...emptyUserState }));
+    navigate("/");
+  };
 
-    return (
-        <div><nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
         <div className="container-fluid">
           <NavLink className="nav-link" aria-current="page" to="/">
-          <img src={Logo} style = {{ height: "40px" }} className="m-1" />
+            <img src={Logo} style={{ height: "40px" }} className="m-1" />
           </NavLink>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100">
               <li className="nav-item">
-                <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+                <NavLink className="nav-link" aria-current="page" to="/">
+                  Home
+                </NavLink>
               </li>
+              {userData.role == SD_Roles.ADMIN ? (
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Admin Panel
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li
+                      className="dropdown-item"
+                      style={{ cursor: "pointer" }}
+                      onClick={() => navigate("/order/myorders")}
+                    >
+                      My Orders
+                    </li>
+                    <li
+                      className="dropdown-item"
+                      style={{ cursor: "pointer" }}
+                      onClick={() => navigate("/order/allorders")}
+                    >
+                      All Orders
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              ) : (
+                <li className="nav-item">
+                  <NavLink
+                    className="nav-link"
+                    aria-current="page"
+                    to="/order/myorders"
+                  >
+                    My Orders
+                  </NavLink>
+                </li>
+              )}
               <li className="nav-item">
-                <NavLink className="nav-link" aria-current="page" to="/ShoppingCart">
+                <NavLink
+                  className="nav-link"
+                  aria-current="page"
+                  to="/ShoppingCart"
+                >
                   <i className="bi bi-cart"></i> {"  "}
                   {userData.id && `${shoppingCartFromStore.length}`}
                 </NavLink>
               </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" aria-current="page" to="/order/myorders">My Orders</NavLink>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Admin Panel
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
               <div className="d-flex" style={{ marginLeft: "auto" }}>
-
-
                 {/* authenticated user  */}
                 {userData.id && (
-                <>
-                  <li className="nav-item">
-                    <button className="nav-link active"
-                    style={{
-                      userSelect: "none",
-                      background: "transparent",
-                      border: 0,
-                    }}
-                    >
-                      Welcome, {userData.fullName}
-                    </button>
-                  </li>
+                  <>
+                    <li className="nav-item">
+                      <button
+                        className="nav-link active"
+                        style={{
+                          userSelect: "none",
+                          background: "transparent",
+                          border: 0,
+                        }}
+                      >
+                        Welcome, {userData.fullName}
+                      </button>
+                    </li>
 
-                  <li className="nav-item">
-                    <button 
-                    className="btn btn-success btn-outlined rounded-pill text-white mx-2"
-                    style={{
-                      border: "none",
-                      height: "40px",
-                      width: "100px",
-                    }}
-                    onClick={handleLogout}
-                    >
-                      Logout
-                    </button>
-                  </li>
-                </>
+                    <li className="nav-item">
+                      <button
+                        className="btn btn-success btn-outlined rounded-pill text-white mx-2"
+                        style={{
+                          border: "none",
+                          height: "40px",
+                          width: "100px",
+                        }}
+                        onClick={handleLogout}
+                      >
+                        Logout
+                      </button>
+                    </li>
+                  </>
                 )}
 
                 {/* login and register for not auth  */}
                 {!userData.id && (
-                <>
-                  <li className="nav-item text-white">
-                    <NavLink className="btn btn-success btn-outlined rounded-pill text-white mx-2"
-                    style={{
-                      border: "none",
-                      height: "40px",
-                      width: "100px",
-                    }} to="/Login">
-                      Login 
-                    </NavLink>
-                  </li>
-                  <li className="nav-item text-white">
-                    <NavLink className="btn btn-success btn-outlined rounded-pill text-white mx-2"
-                    style={{
-                      border: "none",
-                      height: "40px",
-                      width: "100px",
-                    }} to="/Register">
-                      Register 
-                    </NavLink>
-                  </li>
-                </>
+                  <>
+                    <li className="nav-item text-white">
+                      <NavLink
+                        className="btn btn-success btn-outlined rounded-pill text-white mx-2"
+                        style={{
+                          border: "none",
+                          height: "40px",
+                          width: "100px",
+                        }}
+                        to="/Login"
+                      >
+                        Login
+                      </NavLink>
+                    </li>
+                    <li className="nav-item text-white">
+                      <NavLink
+                        className="btn btn-success btn-outlined rounded-pill text-white mx-2"
+                        style={{
+                          border: "none",
+                          height: "40px",
+                          width: "100px",
+                        }}
+                        to="/Register"
+                      >
+                        Register
+                      </NavLink>
+                    </li>
+                  </>
                 )}
-                
               </div>
             </ul>
           </div>
         </div>
       </nav>
     </div>
-    )
-}
+  );
+};
 
 export default Header;
