a58fcec555d34bd501a50e3304bd4615c01bae6f
nde95 <ndennison268@gmail.com>
2023-07-25 15:07:26 -0400

    put request for updating order status

--- src/APIs/orderApi.ts
+++ src/APIs/orderApi.ts
@@ -1,40 +1,56 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const orderApi = createApi({
-    reducerPath:"orderApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl:"https://ndebookstoreapi.azurewebsites.net/api/"
+  reducerPath: "orderApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://ndebookstoreapi.azurewebsites.net/api/",
+  }),
+  tagTypes: ["Orders"],
+  endpoints: (builder) => ({
+    createOrder: builder.mutation({
+      query: (orderDetails) => ({
+        url: "order",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: orderDetails,
+      }),
+      invalidatesTags: ["Orders"],
     }),
-    tagTypes: ["Orders"],
-    endpoints: (builder) => ({
-        createOrder: builder.mutation({
-            query: (orderDetails) => ({
-                url:"order",
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: orderDetails,
-            }),
-            invalidatesTags:["Orders"]
-        }),
-        getAllOrders: builder.query({
-            query: (userId) => ({
-                url:"order",
-                params: {
-                    userId: userId,
-                }
-            }),
-            providesTags:["Orders"]
-        }),
-        getOrderDetails: builder.query({
-            query: (id) => ({
-              url: `order/${id}`,
-            }),
-            providesTags: ["Orders"],
-          }),
+    getAllOrders: builder.query({
+      query: (userId) => ({
+        url: "order",
+        params: {
+          userId: userId,
+        },
+      }),
+      providesTags: ["Orders"],
     }),
+    getOrderDetails: builder.query({
+      query: (id) => ({
+        url: `order/${id}`,
+      }),
+      providesTags: ["Orders"],
+    }),
+    updateOrderDetails: builder.mutation({
+      query: (orderDetails) => ({
+        url: "order/" + orderDetails.orderHeaderId,
+        method: "PUT",
+        header: {
+          "Content-Type": "application/json",
+        },
+        body: orderDetails,
+      }),
+      invalidatesTags: ["Orders"],
+    }),
+  }),
 });
 
-export const { useCreateOrderMutation, useGetAllOrdersQuery, useGetOrderDetailsQuery } = orderApi;
+export const {
+  useCreateOrderMutation,
+  useGetAllOrdersQuery,
+  useGetOrderDetailsQuery,
+  useUpdateOrderDetailsMutation,
+} = orderApi;
 export default orderApi;
