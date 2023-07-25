fa9551f55b1efe4215adacaca960bc11a3bf8233
nde95 <ndennison268@gmail.com>
2023-07-25 14:44:40 -0400

    logic for order status in admin panel

--- src/Components/Page/Order/OrderSummary.tsx
+++ src/Components/Page/Order/OrderSummary.tsx
@@ -1,30 +1,58 @@
+import { useSelector } from "react-redux";
+import { getStatusColor } from "../../../Helper";
 import { cartItemModel } from "../../../Interfaces";
+import { SD_Roles, SD_Status } from "../../../Utils/SD";
 import { orderSummaryProps } from "./orderSummaryProps";
+import { useNavigate } from "react-router-dom";
+import { RootState } from "../../../Storage/Redux/store";
 
-const OrderSummary = ({data, userInput}: orderSummaryProps) => {
-    return (
-        <div>
+const OrderSummary = ({ data, userInput }: orderSummaryProps) => {
+  const badgeTypeColor = getStatusColor(data.status!);
+  const navigate = useNavigate();
+  const userData = useSelector((state: RootState) => state.authStore);
+
+  const nextStatus: any =
+    data.status! === SD_Status.CONFIRMED
+      ? { color: "info", value: SD_Status.HANDED_TO_CARRIER }
+      : data.status! === SD_Status.HANDED_TO_CARRIER
+      ? { color: "warning", value: SD_Status.SHIPPED }
+      : data.status! === SD_Status.SHIPPED && {
+          color: "success",
+          value: SD_Status.DELIVERED,
+        };
+
+  return (
+    <div>
       {" "}
-      <h3 className="text-success">Order Summary</h3>
+      <div className="d-flex justify-content-between align-items-center">
+        <h3 className="text-success">Order Summary</h3>
+        <span className={`btn btn-outline-${badgeTypeColor} fs-6`}>
+          {data.status}
+        </span>
+      </div>
       <div className="mt-3">
         <div className="border py-3 px-2">Name : {userInput.name}</div>
         <div className="border py-3 px-2">Email : {userInput.email}</div>
         <div className="border py-3 px-2">Phone : {userInput.phoneNumber}</div>
         <div className="border py-3 px-2">
-        <h4 className="text-success">Books</h4> 
-        <div className="p-3">
-            {data.cartItems?.map((cartItem: cartItemModel, index:number) => {
-                 return(
-                    <div className="d-flex" key={index}>
-                    <div className="d-flex w-100 justify-content-between">
+          <h4 className="text-success">Books</h4>
+          <div className="p-3">
+            {data.cartItems?.map((cartItem: cartItemModel, index: number) => {
+              return (
+                <div className="d-flex" key={index}>
+                  <div className="d-flex w-100 justify-content-between">
                     <p>{cartItem.productItem?.name}</p>
-                    <p>${cartItem.productItem?.price} x {cartItem.quantity} =</p>
-                    </div>
-                    <p style={{ width: "70px", textAlign: "right" }}>$
-                    {(cartItem.productItem?.price ?? 0) * (cartItem.quantity ?? 0)}
+                    <p>
+                      ${cartItem.productItem?.price} x {cartItem.quantity} =
                     </p>
+                  </div>
+                  <p style={{ width: "70px", textAlign: "right" }}>
+                    $
+                    {(cartItem.productItem?.price ?? 0) *
+                      (cartItem.quantity ?? 0)}
+                  </p>
                 </div>
-            )
+              );
             })}
 
             <hr />
@@ -34,8 +62,21 @@ const OrderSummary = ({data, userInput}: orderSummaryProps) => {
           </div>
         </div>
       </div>
+      <div className="d-flex justify-content-between align-items-center mt-5">
+        <button className="btn btn-secondary" onClick={() => navigate(-1)}>
+          Back To Orders
+        </button>
+        {userData.role == SD_Roles.ADMIN && (
+          <div className="d-flex">
+            <button className="btn btn-danger mx-2">Cancel</button>
+            <button className={`btn btn-${nextStatus.color}`}>
+              {nextStatus.value}
+            </button>
+          </div>
+        )}
+      </div>
     </div>
-    );
+  );
 };
 
-export default OrderSummary; 
+export default OrderSummary;
