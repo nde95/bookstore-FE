f758db171e4a934b5d143d852d7aac27fbb18990
nde95 <ndennison268@gmail.com>
2023-07-27 11:22:12 -0400

    updating for new input types

--- src/Helper/inputHelper.ts
+++ src/Helper/inputHelper.ts
@@ -1,7 +1,12 @@
-const InputHelper = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>, data: any) => {
-    const tempData: any = {...data};
-    tempData[e.target.name] = e.target.value
-    return tempData;
-}
+const InputHelper = (
+  e: React.ChangeEvent<
+    HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+  >,
+  data: any
+) => {
+  const tempData: any = { ...data };
+  tempData[e.target.name] = e.target.value;
+  return tempData;
+};
 
 export default InputHelper;
