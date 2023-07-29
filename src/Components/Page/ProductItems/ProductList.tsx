70d8848bdebf2e74a3eb32bc7eb3578559d9b0bf
nde95 <ndennison268@gmail.com>
2023-07-29 12:21:22 -0400

    handling category and text filters

--- src/Components/Page/ProductItems/ProductList.tsx
+++ src/Components/Page/ProductItems/ProductList.tsx
@@ -9,6 +9,8 @@ import { RootState } from "../../../Storage/Redux/store";
 
 const ProductList = () => {
   const [productItems, setProductItems] = useState<productsModel[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [categoryList, setCategoryList] = useState([""]);
   const dispatch = useDispatch();
   const { data, isLoading } = useGetProductItemsQuery(null);
 
@@ -18,7 +20,7 @@ const ProductList = () => {
 
   useEffect(() => {
     if (data && data.result) {
-      const tempProductsArray = handleFilters(searchValue);
+      const tempProductsArray = handleFilters(selectedCategory, searchValue);
       setProductItems(tempProductsArray);
     }
   }, [searchValue]);
@@ -27,11 +29,45 @@ const ProductList = () => {
     if (!isLoading) {
       dispatch(setProductItem(data.result));
       setProductItems(data.result);
+      const tempCategoryList = ["All"];
+      data.result.forEach((item: productsModel) => {
+        if (tempCategoryList.indexOf(item.category) === -1) {
+          tempCategoryList.push(item.category);
+        }
+      });
+
+      setCategoryList(tempCategoryList);
     }
   }, [isLoading]);
 
-  const handleFilters = (search: string) => {
-    let tempProducts = [...data.result];
+  const handleCategoryClick = (i: number) => {
+    const buttons = document.querySelectorAll(".custom-buttons");
+    let localCategory;
+    buttons.forEach((button, index) => {
+      if (index === i) {
+        button.classList.add("active");
+        if (index === 0) {
+          localCategory = "All";
+        } else {
+          localCategory = categoryList[index];
+        }
+        setSelectedCategory(localCategory);
+        const tempArray = handleFilters(localCategory, searchValue);
+        setProductItems(tempArray);
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  };
+
+  const handleFilters = (category: string, search: string) => {
+    let tempProducts =
+      category === "All"
+        ? [...data.result]
+        : data.result.filter(
+            (item: productsModel) =>
+              item.category.toUpperCase() === category.toUpperCase()
+          );
 
     if (search) {
       const tempSearchProducts = [...tempProducts];
@@ -49,6 +85,22 @@ const ProductList = () => {
 
   return (
     <div className="container row">
+      <div className="my-3">
+        <ul className="nav w-100 d-flex justify-content-center">
+          {categoryList.map((categoryName, index) => (
+            <li className="nav-item" key={index}>
+              <button
+                className={`nav-link p-0 pb-2 custom-buttons fs-5 ${
+                  index === 0 && "active"
+                }`}
+                onClick={() => handleCategoryClick(index)}
+              >
+                {categoryName}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
       {productItems.length > 0 &&
         productItems.map((productItem: productsModel, index: number) => (
           <ProductCard productItem={productItem} key={index} />
