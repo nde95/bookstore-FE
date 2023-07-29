9aa7de73e43e8fb536fbafe0c6764bcc7cff6667
nde95 <ndennison268@gmail.com>
2023-07-29 10:49:34 -0400

    home banner component

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,13 +1,15 @@
+import { Banner } from "../Components/Page/Common";
 import { ProductList } from "../Components/Page/ProductItems";
 
 const Home = () => {
-    return (
-        <div>
-            <div className="container p-2">
-                <ProductList />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Banner />
+      <div className="container p-2">
+        <ProductList />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
